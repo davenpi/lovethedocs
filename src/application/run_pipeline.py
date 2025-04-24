ec7139a7eab53ebbfea7ee090202825fe5e3d2d5
davenpi <davenport.ianc@gmail.com>
2025-04-24 13:55:21 -0400

    Add ports and remove relative imports in pipeline.

--- src/application/run_pipeline.py
+++ src/application/run_pipeline.py
@@ -1,32 +1,35 @@
 """
-High-level orchestration. Glue code only.
+Glue code to run the pipeline.
 """
 
 from pathlib import Path
 from typing import Sequence
 
-from src.gateways import file_system, openai_client, schema_loader
-from . import config, mappers, prompt_builder, services
+from src import ports
+from src.application import config, mappers, prompt_builder, services
+from src.gateways import file_system as fs_gateway
+from src.gateways import openai_client as ai_gateway
+from src.gateways import schema_loader
 
 
-def run_pipeline(paths: Sequence[str], settings: config.Settings = config.Settings()):
+def run_pipeline(
+    paths: Sequence[str],
+    *,
+    settings: config.Settings = config.Settings(),
+    ai_client: ports.AIClientPort = ai_gateway,  # defaults to real gateway
+    file_writer: ports.FileWriterPort = fs_gateway,  # defaults to real gateway
+):
     validator = schema_loader.VALIDATOR
 
     for raw_path in paths:
         base = Path(raw_path)
-        modules = file_system.load_modules(base)  # dict[module_path, code]
+        modules = file_writer.load_modules(base)
         prompts = prompt_builder.build_prompts(modules)
 
         for path, prompt in prompts.items():
-            print("-" * 80)
-            print(f"Prompt for {path}:\n {prompt}")
-            resp_json = openai_client.request(prompt, model=settings.model_name)
-            validator.validate(resp_json)  # raise error if invalid
+            resp_json = ai_client.request(prompt, model=settings.model_name)
+            validator.validate(resp_json)
 
-            # module_edit = _create_module_edit_from_json(resp_json)
             module_edit = mappers.create_module_edit_from_json(resp_json)
-            new_code = services.write_new_code(
-                old_code=modules[path], module_edit=module_edit
-            )
-            print(f"New code for {path}: {new_code}")
-            file_system.write_file(path, new_code, root=base)
+            new_code = services.write_new_code(modules[path], module_edit)
+            file_writer.write_file(path, new_code, root=base)
