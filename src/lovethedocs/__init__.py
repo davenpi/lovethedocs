5765ff56d2362093204017f256202875da465440
davenpi <davenport.ianc@gmail.com>
2025-05-09 11:47:42 -0400

    bump version to v0.2.3

--- src/lovethedocs/__init__.py
+++ src/lovethedocs/__init__.py
@@ -1,8 +1,7 @@
 """
-LoveTheDocs: Enhance Python docs with AI
+lovethedocs: Enhance Python docs with AI
 
-Make your code more understandable, maintainable, and useful with
-AI-generated documentation.
+Make every docstring sparkle.
 """
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
